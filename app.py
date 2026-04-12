42c14e67ff98bfc5fab90cdc4e5ce3aa730824cb
Ivan Ivanov <ivanov@example.com>
2026-04-12 18:52:37 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def my_function(x, y):
+    return x + y   # замените на любую простую операцию: x - y, x * y, x / y
+
+@app.route('/')
+def home():
+    result = my_function(10, 5)
+    return f"<h1>Результат: {result}</h1>"
+
+if __name__ == '__main__':
+    app.run()
